ac4486c10921bcc82ca74b414fa335bdaa633f1d
rdsnow <rdsnow@163.com>
2018-03-04 08:46:24 +0800

    Add user-tab to project...

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-page">
     <header-bar></header-bar>
-    <h1>This is home page.....</h1>
+    <UserTab></UserTab>
     <FooterBar></FooterBar>
   </div>
 </template>
@@ -9,28 +9,16 @@
 <script>
   import HeaderBar from './HeaderBar'
   import FooterBar from './FooterBar'
+  import UserTab from './UserTab'
 
 
   export default {
-    components: {HeaderBar, FooterBar},
+    components: {HeaderBar, FooterBar,UserTab},
     name: "home-page",
-    created() {
-      this.$http.get("http://jsonplaceholder.typicode.com/users").then((data)=>{
-        console.log(data);
-      })
-    }
   }
 </script>
 
 
 <style scoped>
-  h1 {
-    font-family: "Microsoft YaHei UI Light";
-    width: 1000px;
-    margin: 0 auto;
-    text-align: center;
-    color: red;
-    line-height: 600px;
-    border: 1px solid #dddddd;
-  }
+
 </style>
---
+++ src/components/UserTab.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="user-tab">
+    <table>
+      <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>username</th>
+        <th>email</th>
+        <th>phone</th>
+        <th>website</th>
+      </tr>
+      <tbody>
+      <tr v-for="user in users">
+        <td>{{user.id}}</td>
+        <td>{{user.name}}</td>
+        <td>{{user.username}}</td>
+        <td>{{user.email}}</td>
+        <td>{{user.phone}}</td>
+        <td>{{user.website}}</td>
+      </tr>
+      </tbody>
+    </table>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "user-tab",
+    data: function () {
+      return {
+        users: {}
+      };
+    },
+    created() {
+      this.$http.get("http://jsonplaceholder.typicode.com/users").then((data) => {
+        this.users = data.body;
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .user-tab {
+    font-family: "Microsoft YaHei UI Light";
+    width: 1000px;
+    margin: 0 auto;
+    text-align: center;
+    color: blue;
+    height: 600px;
+    border: 1px solid #dddddd;
+  }
+  table{
+    margin: auto;
+    border: 1px;
+  }
+
+</style>
